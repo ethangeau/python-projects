75d562eaa56c77cd56fae04563de3a02afbe01da
yebinge <yebing@student.unimelb.au>
2022-01-18 15:24:51 +0800

    Get top100 movies using web scraping.

---
+++ 100-movies-to-watch-start/main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+original_URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(URL)
+contents = response.text
+# print(contents)
+
+soup = BeautifulSoup(contents, 'html.parser')
+# print(soup.prettify())
+all_movies = soup.find_all(name="h3")
+# print(all_movies)
+movies = [movie.text for movie in all_movies]
+# print(movies)
+movies_reverse = movies[::-1]
+
+with open("top100-movies.csv", mode='w') as file:
+    for movie in movies_reverse:
+        file.write(f"{movie}\n")
